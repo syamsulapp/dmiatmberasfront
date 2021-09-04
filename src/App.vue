45349aae6abc7bc0738decfee3da8206cc8a7252
syamsulapp <samsul.marif100@gmail.com>
2021-09-04 08:32:35 +0800

    front end atm beras

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/dmi.jpg" />
+  <h1>ATM BERAS DAN TELUR PROVINSI SULAWESI TENGGARA</h1>
+  <div class="container my-5">
+    <div class="row justify-content-center">
+      <div class="col-8">
+        <div class="card rounded shadow">
+          <div class="card-header">HASIL DATA</div>
+          <div class="card-body">
+            <table class="table">
+              <tr v-for="(atmberas, index) in atmberas" :key="index">
+                <td v-if="atmberas.beras != 0">
+                  {{ atmberas.beras }} {{ "liter" }}
+                </td>
+                <td v-if="atmberas.telur && atmberas.telur_beras != 0">
+                  {{ atmberas.telur_beras }} {{ "liter" }} {{ atmberas.telur }}
+                  {{ "telur" }}
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import { onMounted, ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  setup() {
+    // reactive state (mengumpulkan/menyimpan semua data api kedalam array)
+    let atmberas = ref([]);
 
+    onMounted(() => {
+      // fungsinya on mount untuk mengambil data enpoint API
+      axios
+        .get("http://192.168.1.10/BackendAtmBerasUMK/public/api/getApiTelur")
+        .then((result) => {
+          atmberas.value = result.data;
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+    });
+
+    return {
+      atmberas,
+    };
+  },
+};
+setTimeout(function () {
+  location.reload();
+}, 2000);
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import jQuery from 'jquery';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
+
+window.jQuery = jQuery;
 
 createApp(App).mount('#app')
